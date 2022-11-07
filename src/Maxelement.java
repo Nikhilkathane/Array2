491501f9d3f8171fc11ce31a193fbaec8cd39435
nikhil kathane <nikhilkathane97@gmail.com>
2022-11-07 11:41:32 +0530

    Initial commit

---
+++ src/Maxelement.java
@@ -0,0 +1,13 @@
+
+public class Maxelement {
+  public static void main(String[] args)
+  {
+                 int a[]={22,1,2,3,500,4,5};
+                 int max=a[0];
+                 for(int i=0;i<a.length;i++)
+                            if(a[i]>max)
+                                max=a[i];
+                 System.out.println(max);
+                }
+}
+
